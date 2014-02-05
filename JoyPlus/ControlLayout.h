1c82c3c2fff6e575c684078a10eb9b0e4a4c83da
Ciel <imwithye@gmail.com>
2014-02-05 18:18:00 +0800

    Abstract class ControlLayout added.

---
+++ JoyPlus/ControlLayout.h
@@ -0,0 +1,18 @@
+//
+//  ControlLayout.h
+//  JoyPlus
+//
+//  Created by Ciel Breiz on 5/2/14.
+//  Copyright 2014 Ciel Breiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface ControlLayout : CCScene {
+    
+}
+
+- (id)init;
+
+@end
--- JoyPlus/ControlLayout.m
+++ JoyPlus/ControlLayout.m
@@ -1,21 +1,16 @@
 //
-//  ConnectScene.m
+//  ControlLayout.m
 //  JoyPlus
 //
 //  Created by Ciel Breiz on 5/2/14.
 //  Copyright 2014 Ciel Breiz. All rights reserved.
 //
 
-#import "ConnectScene.h"
-#import "IntroScene.h"
 #import "ControlLayout.h"
+#import "IntroScene.h"
 
-@implementation ConnectScene
 
-+ (ConnectScene *)scene
-{
-    return [[self alloc] init];
-}
+@implementation ControlLayout
 
 - (id)init
 {
@@ -23,29 +18,22 @@
     self = [super init];
     if (!self) return(nil);
     
-    // Enable touch handling on scene node
-    self.userInteractionEnabled = YES;
+    // Disable touch handling on scene node
+    self.userInteractionEnabled = NO;
     
     // Create a colored background (Dark Grey)
     CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.0f]];
     [self addChild:background];
     
     // Create a back button
-    CCButton *backButton = [CCButton buttonWithTitle:@"[ Menu ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    CCButton *backButton = [CCButton buttonWithTitle:@"[ Back ]" fontName:@"Verdana-Bold" fontSize:18.0f];
     backButton.positionType = CCPositionTypeNormalized;
     backButton.position = ccp(0.85f, 0.95f); // Top Right of screen
     [backButton setTarget:self selector:@selector(onBackClicked:)];
     [self addChild:backButton];
     
-    // Spinning scene button
-    CCButton *spinningButton = [CCButton buttonWithTitle:@"[ Next ]" fontName:@"Verdana-Bold" fontSize:18.0f];
-    spinningButton.positionType = CCPositionTypeNormalized;
-    spinningButton.position = ccp(0.5f, 0.15f);
-    [spinningButton setTarget:self selector:@selector(onNextClicked:)];
-    [self addChild:spinningButton];
-    
-    // done
-	return self;
+    // Done
+    return self;
 }
 
 - (void)onBackClicked:(id)sender
@@ -53,11 +41,6 @@
     // back to intro scene with transition
     [[CCDirector sharedDirector] replaceScene:[IntroScene scene]
                                withTransition:[CCTransition transitionPushWithDirection:CCTransitionDirectionRight duration:1.0f]];
-}
-
-- (void)onNextClicked:(id)sender
-{
-    [[CCDirector sharedDirector] replaceScene:[[ControlLayout alloc] init] withTransition:[CCTransition transitionPushWithDirection:CCTransitionDirectionRight duration:1.0f]];
 }
 
 @end
