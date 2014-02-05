ef9870437c9020527696b11b6d63c7823d67bf42
Ciel <imwithye@gmail.com>
2014-02-05 17:33:51 +0800

    Initial Commit

---
+++ JoyPlus/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  JoyPlus
+//
+//  Created by Ciel Breiz on 5/2/14.
+//  Copyright Ciel Breiz 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ JoyPlus/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JoyPlus
+//
+//  Created by Ciel Breiz on 5/2/14.
+//  Copyright Ciel Breiz 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
