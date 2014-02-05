6ac1f94f02f426899d30648557969faa520a2b15
Ciel <imwithye@gmail.com>
2014-02-05 17:45:39 +0800

    Create ConnectScene class.

---
+++ JoyPlus/ConnectScene.h
@@ -0,0 +1,19 @@
+//
+//  ConnectScene.h
+//  JoyPlus
+//
+//  Created by Ciel Breiz on 5/2/14.
+//  Copyright 2014 Ciel Breiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface ConnectScene : CCScene {
+    
+}
+
++ (ConnectScene *)scene;
+- (id)init;
+
+@end
---
+++ JoyPlus/ConnectScene.m
@@ -0,0 +1,50 @@
+//
+//  ConnectScene.m
+//  JoyPlus
+//
+//  Created by Ciel Breiz on 5/2/14.
+//  Copyright 2014 Ciel Breiz. All rights reserved.
+//
+
+#import "ConnectScene.h"
+#import "IntroScene.h"
+
+@implementation ConnectScene
+
++ (ConnectScene *)scene
+{
+    return [[self alloc] init];
+}
+
+- (id)init
+{
+    // Apple recommend assigning self with supers return value
+    self = [super init];
+    if (!self) return(nil);
+    
+    // Enable touch handling on scene node
+    self.userInteractionEnabled = YES;
+    
+    // Create a colored background (Dark Grey)
+    CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.0f]];
+    [self addChild:background];
+    
+    // Create a back button
+    CCButton *backButton = [CCButton buttonWithTitle:@"[ Menu ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    backButton.positionType = CCPositionTypeNormalized;
+    backButton.position = ccp(0.85f, 0.95f); // Top Right of screen
+    [backButton setTarget:self selector:@selector(onBackClicked:)];
+    [self addChild:backButton];
+    
+    // done
+	return self;
+}
+
+- (void)onBackClicked:(id)sender
+{
+    // back to intro scene with transition
+    [[CCDirector sharedDirector] replaceScene:[IntroScene scene]
+                               withTransition:[CCTransition transitionPushWithDirection:CCTransitionDirectionRight duration:1.0f]];
+}
+
+@end
